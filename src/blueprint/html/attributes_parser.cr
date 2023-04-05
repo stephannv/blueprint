58d0dbce157fad581dcf7d363856274f7c143f41
Stephann Vasconcelos <3025661+stephannv@users.noreply.github.com>
2023-04-05 18:31:11 -0300

    refactor: Minor code refactor

--- src/blueprint/html/attributes_parser.cr
+++ src/blueprint/html/attributes_parser.cr
@@ -9,30 +9,35 @@ module Blueprint::HTML
 
   private def append_attribute(io : String::Builder, attribute_name, attribute_value) : Nil
     case attribute_value
+    when Nil, false
+      # does nothing
     when true
       append_boolean_attribute(io, attribute_name)
     when NamedTuple
       process_named_tuple_attribute(io, attribute_name, attribute_value)
-    when Nil, false
-      # does nothing
     else
       append_normal_attribute(io, attribute_name, attribute_value)
     end
   end
 
   private def append_normal_attribute(io : String::Builder, attribute_name, attribute_value) : Nil
-    io << " " << parse_attribute_name(attribute_name) << "=\""
+    io << " "
+    io << parse_attribute_name(attribute_name)
+    io << %(=")
     ::HTML.escape(attribute_value.to_s, io)
-    io << "\""
+    io << %(")
   end
 
   private def append_boolean_attribute(io : String::Builder, attribute_name) : Nil
-    io << " " << parse_attribute_name(attribute_name)
+    io << " "
+    io << parse_attribute_name(attribute_name)
   end
 
   private def process_named_tuple_attribute(io : String::Builder, attribute_name, attribute_value : NamedTuple) : Nil
+    attribute_name_prefix = parse_attribute_name(attribute_name)
+
     attribute_value.each do |name, value|
-      append_attribute(io, "#{parse_attribute_name(attribute_name)}-#{parse_attribute_name(name)}", value)
+      append_attribute(io, "#{attribute_name_prefix}-#{parse_attribute_name(name)}", value)
     end
   end
 
