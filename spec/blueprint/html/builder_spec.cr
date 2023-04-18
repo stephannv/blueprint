aa6fd3ec38b4554af8b1a6189ec3268523ad399c
Stephann Vasconcelos <3025661+stephannv@users.noreply.github.com>
2023-04-17 21:49:28 -0300

    feat: Add experimental Blueprint::HTML.builder

---
+++ spec/blueprint/html/builder_spec.cr
@@ -0,0 +1,17 @@
+require "../../spec_helper"
+
+describe Blueprint::HTML do
+  describe ".build" do
+    it "renders given html structure" do
+      html = Blueprint::HTML.build do
+        h1 { "Hello" }
+
+        div do
+          span { "World" }
+        end
+      end
+
+      html.should eq "<h1>Hello</h1><div><span>World</span></div>"
+    end
+  end
+end
