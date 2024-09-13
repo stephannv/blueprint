0e4c2a72d2d06335cb08b5d4bf36c84c60ca6fd8
stephann <3025661+stephannv@users.noreply.github.com>
2024-09-13 18:35:19 -0300

    refactor: Rename #element argument

--- src/blueprint/html/element_renderer.cr
+++ src/blueprint/html/element_renderer.cr
@@ -1,29 +1,29 @@
 module Blueprint::HTML::ElementRenderer
-  private def element(_tag_name : String | Symbol, **attributes, &) : Nil
+  private def element(tag_name : String | Symbol, **attributes, &) : Nil
     @buffer << "<"
-    @buffer << _tag_name
+    @buffer << tag_name
     append_attributes(attributes)
     @buffer << ">"
     capture_content { yield }
     @buffer << "</"
-    @buffer << _tag_name
+    @buffer << tag_name
     @buffer << ">"
   end
 
-  private def element(_tag_name : String | Symbol, __content__ : String, **attributes) : Nil
+  private def element(tag_name : String | Symbol, __content__ : String, **attributes) : Nil
     @buffer << "<"
-    @buffer << _tag_name
+    @buffer << tag_name
     append_attributes(attributes)
     @buffer << ">"
     ::HTML.escape(__content__, @buffer)
     @buffer << "</"
-    @buffer << _tag_name
+    @buffer << tag_name
     @buffer << ">"
   end
 
-  private def void_element(_tag_name : String | Symbol, **attributes) : Nil
+  private def void_element(tag_name : String | Symbol, **attributes) : Nil
     @buffer << "<"
-    @buffer << _tag_name
+    @buffer << tag_name
     append_attributes(attributes)
     @buffer << ">"
   end
