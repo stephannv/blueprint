f2f1fe58ef51f1fbc6a31ef8cf65226cfd6d2087
stephann <3025661+stephannv@users.noreply.github.com>
2024-09-14 14:26:03 -0300

    feat: Remove Blueprint::RawHTML module (#72)

--- benchmark/benchmark.cr
+++ benchmark/benchmark.cr
@@ -1,19 +1,16 @@
 require "benchmark"
 
 require "./blueprint_html"
-require "./blueprint_raw_html"
 require "./ecr"
 
 require "../src/blueprint/version"
 
 blueprint_html = BlueprintHTML::Page.new.to_s
-blueprint_raw_html = BlueprintRawHTML::Page.new.to_s
-ecr = ECR::Page.new.to_s # ECR is here just to have a base value to compare
+ecr = ECR::Page.new.to_s.chomp # ECR is here just to have a base value to compare
 
-raise "Different results" if blueprint_html != blueprint_raw_html && blueprint_html != ecr
+raise "Different results" if blueprint_html != ecr
 
 Benchmark.ips do |x|
   x.report("Blueprint::HTML #{Blueprint::VERSION}") { BlueprintHTML::Page.new.to_s }
-  x.report("Blueprint::RawHTML #{Blueprint::VERSION}") { BlueprintRawHTML::Page.new.to_s }
   x.report("ECR") { ECR::Page.new.to_s }
 end
