2192bbad66d12e50503bfefbd7dfd8c65690a345
stephann <3025661+stephannv@users.noreply.github.com>
2024-10-14 13:41:41 -0300

    feat: Add escape_once helper (#83)

--- spec/spec_helper.cr
+++ spec/spec_helper.cr
@@ -1,6 +1,14 @@
 require "spec"
 require "../src/blueprint/html"
 
+class MarkdownLink
+  def initialize(@text : String, @href : String); end
+
+  def to_s
+    "[#{@text}](#{@href})"
+  end
+end
+
 def normalize_html(html : String) : String
   html.strip.gsub(/\R\s+/, "")
 end
