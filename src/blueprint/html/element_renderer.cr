75804efd533ce81940d1014d14e211d10eae1cb7
stephann <3025661+stephannv@users.noreply.github.com>
2025-01-19 16:29:22 -0300

    Remove unused code (#99)

--- src/blueprint/html/element_renderer.cr
+++ src/blueprint/html/element_renderer.cr
@@ -10,17 +10,6 @@ module Blueprint::HTML::ElementRenderer
     @buffer << ">"
   end
 
-  private def element(tag_name : String | Symbol, __content__, **attributes) : Nil
-    @buffer << "<"
-    @buffer << tag_name
-    __append_attributes__(attributes)
-    @buffer << ">"
-    __append_to_buffer__(__content__)
-    @buffer << "</"
-    @buffer << tag_name
-    @buffer << ">"
-  end
-
   private def void_element(tag_name : String | Symbol, **attributes) : Nil
     @buffer << "<"
     @buffer << tag_name
