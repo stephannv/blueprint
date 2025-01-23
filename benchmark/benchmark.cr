3c3d09b872b0d613d1ebe6b94220259b00e0b992
stephann <3025661+stephannv@users.noreply.github.com>
2025-01-23 18:19:40 -0300

    Revert "Add attribute caching (#98)" (#101)
    This reverts commit 39f6d4d3da8bd7315465a08b5e604a7ae5e335b1.

--- benchmark/benchmark.cr
+++ benchmark/benchmark.cr
@@ -1,19 +1,16 @@
 require "benchmark"
 
 require "./blueprint_html"
-require "./cached_blueprint_html"
 require "./ecr"
 
 require "../src/blueprint/version"
 
 blueprint_html = BlueprintHTML::Page.new.to_s
-cached_blueprint_html = BlueprintHTML::PageWithCache.new.to_s
 ecr = ECR::Page.new.to_s.chomp # ECR is here just to have a base value to compare
 
-raise "Different results" if blueprint_html != ecr || blueprint_html != cached_blueprint_html
+raise "Different results" if blueprint_html != ecr
 
 Benchmark.ips do |x|
-  x.report("ECR") { ECR::Page.new.to_s }
-  x.report("Cached Blueprint::HTML #{Blueprint::VERSION}") { BlueprintHTML::PageWithCache.new.to_s }
   x.report("Blueprint::HTML #{Blueprint::VERSION}") { BlueprintHTML::Page.new.to_s }
+  x.report("ECR") { ECR::Page.new.to_s }
 end
