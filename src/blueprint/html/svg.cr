89e282f9b2fc0590e06fd8818f180e82e6b42f05
stephann <3025661+stephannv@users.noreply.github.com>
2025-01-24 17:59:18 -0300

    Refactor SVG

--- src/blueprint/html/svg.cr
+++ src/blueprint/html/svg.cr
@@ -1,38 +1,20 @@
-module Blueprint::HTML::SVG
-  private def svg(**attributes) : Nil
-    svg(**attributes) { }
+struct Blueprint::HTML::SVG(T)
+  include Blueprint::HTML
+
+  @attributes : T
+
+  macro finished
+    {% for tag in %w(a animate animateMotion animateTransform circle clipPath defs desc discard ellipse feBlend feColorMatrix feComponentTransfer feComposite feConvolveMatrix feDiffuseLighting feDisplacementMap feDistantLight feDropShadow feFlood feFuncA feFuncB feFuncG feFuncR feGaussianBlur feImage feMerge feMergeNode feMorphology feOffset fePointLight feSpecularLighting feSpotLight feTile feTurbulence filter foreignObject g image line linearGradient marker mask metadata mpath path pattern polygon polyline radialGradient rect script set stop style svg switch symbol text textPath title tspan use view) %}
+      register_element {{tag}}
+    {% end %}
   end
 
-  private def svg(**attributes, &) : Nil
-    render SVGComponent.new(**attributes) do |component|
-      with component yield
-    end
+  def initialize(**@attributes : **T)
   end
 
-  private struct SVGComponent(T)
-    include Blueprint::HTML
-
-    @attributes : T
-
-    def self.new(**kwargs) : SVGComponent
-      new kwargs
-    end
-
-    macro finished
-      {% for tag in %w(a animate animateMotion animateTransform circle clipPath defs desc discard ellipse feBlend feColorMatrix feComponentTransfer feComposite feConvolveMatrix feDiffuseLighting feDisplacementMap feDistantLight feDropShadow feFlood feFuncA feFuncB feFuncG feFuncR feGaussianBlur feImage feMerge feMergeNode feMorphology feOffset fePointLight feSpecularLighting feSpotLight feTile feTurbulence filter foreignObject g image line linearGradient marker mask metadata mpath path pattern polygon polyline radialGradient rect script set stop style svg switch symbol text textPath title tspan use view) %}
-        register_element {{tag}}
-      {% end %}
-    end
-
-    def initialize(attributes : T)
-      {% T.raise "Expected T be NamedTuple, but got #{T}." unless T <= NamedTuple %}
-      @attributes = attributes
-    end
-
-    private def blueprint(&) : Nil
-      element :svg, **@attributes do
-        yield
-      end
+  private def blueprint(&) : Nil
+    element :svg, **@attributes do
+      yield
     end
   end
 end
